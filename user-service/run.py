2317df0f2087cf0bfcf67d37a888fedf9304627f
Faruq <faruqabdulsalam@yahoo.com>
2022-04-05 16:33:55 +0100

    Create base script for user service

--- user-service/run.py
+++ user-service/run.py
@@ -1,1 +1,11 @@
-from app import app
+import os
+from dotenv import load_dotenv 
+from app import create_app
+
+load_dotenv()
+
+app = create_app()
+
+if __name__ == '__main__':
+	port = int(os.environ.get('PORT'))
+	app.run(host='0.0.0.0', port=port)
