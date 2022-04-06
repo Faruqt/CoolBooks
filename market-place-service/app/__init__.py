998665c8007c651c6bdccca1664f28d15d0013cb
Faruq <faruqabdulsalam@yahoo.com>
2022-04-06 21:12:33 +0100

    Add sql alchemy to market place init script

--- market-place-service/app/__init__.py
+++ market-place-service/app/__init__.py
@@ -1,9 +1,17 @@
 from flask import Flask
+from .models import db
+from . import config
 
 def create_app():
     app = Flask(__name__)
+    app.config['SQLALCHEMY_DATABASE_URI'] = config.DATABASE_CONNECTION_URI
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+    db.init_app(app)
 
     with app.app_context():
+        db.create_all()
         from .api import mb as market_blueprint
         app.register_blueprint(market_blueprint)
         return app
+
