75073a162eb65dfce333eeaaa5ef49ea2d756847
Faruq <faruqabdulsalam@yahoo.com>
2022-04-06 21:11:56 +0100

    Add sql alchemy to catalogue init script

--- catalogue-service/app/__init__.py
+++ catalogue-service/app/__init__.py
@@ -1,9 +1,17 @@
 from flask import Flask
+from .models import db
+from . import config
 
 def create_app():
     app = Flask(__name__)
+    app.config['SQLALCHEMY_DATABASE_URI'] = config.DATABASE_CONNECTION_URI
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+    db.init_app(app)
 
     with app.app_context():
+        db.create_all()
         from .api import cb as catalogue_blueprint
         app.register_blueprint(catalogue_blueprint)
         return app
+
