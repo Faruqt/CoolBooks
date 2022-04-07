397655f49dbde6c94da515e3ac0a3e1d26811ecc
Faruq <faruqabdulsalam@yahoo.com>
2022-04-08 00:03:58 +0100

    Create api endpoint to confirm user identity

--- user-service/app/api/auth.py
+++ user-service/app/api/auth.py
@@ -1,3 +1,4 @@
+import json
 from . import ub
 from ..models import User
 from .. import db
@@ -5,7 +6,7 @@ from .errors import bad_request
 from datetime import datetime, timedelta, timezone
 from flask import make_response, request, jsonify
 from flask_jwt_extended import create_access_token,get_jwt,get_jwt_identity, \
-							   set_access_cookies, unset_jwt_cookies
+							   set_access_cookies, unset_jwt_cookies,jwt_required
 
 
 @ub.after_request
@@ -27,10 +28,12 @@ def refresh_expiring_jwts(response):
 
 @ub.route('/api/user/create', methods=['POST'])
 def post_register():
-    data = request.get_json()
+    data = request.form.to_dict() or {}
 
-    if data['username'] == '' or data['email'] == '' or data['password'] == '':
-        return bad_request('Please make sure all fields are filled in correctly')
+    for field in ['username', 'email', 'first_name', 'last_name', 'password']:
+        if field not in data:
+            return bad_request('Please make sure all fields are filled in correctly')
+
     if User.query.filter_by(email=data['email'].lower()).first():
         return bad_request('email address already registered')
     if User.query.filter_by(username=data['username'].lower()).first():
@@ -46,25 +49,39 @@ def post_register():
     db.session.add(user)
     db.session.commit()
 
-    response = user.to_dict()
+    response = user.to_json()
     status_code = 201
 
     return make_response(response, status_code)
 
 @ub.route('/api/user/login', methods=['POST'])
 def post_login():
-    data = request.get_json() 
+    data = request.form.to_dict() or {}
     email = data['email']
     password = data['password']
+
     user = User.query.filter_by(email=email).first()
     if user is not None and user.check_password(password):
         access_token = create_access_token(identity=email)
-        return make_response(jsonify({'message': 'User logged in', 'access_token': access_token}))
+        return make_response({'message': 'User logged in', 'access_token': access_token})
 
-    return make_response(jsonify({'message': 'User not logged in'}), 401)
+    return make_response({'message': 'User not logged in'}, 401)
 
 @ub.route('/api/user/logout', methods=['POST'])
 def post_logout():
     response = jsonify({"msg": "logout successful"})
     unset_jwt_cookies(response)
     return response
+
+@ub.route('/api/user', methods=['GET'])
+@jwt_required()
+def get_current_user():
+    user_email = get_jwt_identity()
+    if user_email:
+        user = User.query.filter_by(email=user_email).first()
+        if user:
+            return make_response({'result': user.to_json()})
+        else:
+            return make_response({'message': 'User not found'}, 404)
+
+    return make_response({'message': 'Not logged in'}, 401)
