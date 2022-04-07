43349b678f18e0bffea0227b234d0dec8bdd8c5c
Faruq <faruqabdulsalam@yahoo.com>
2022-04-08 00:01:20 +0100

    Add cloudinary secrets

--- catalogue-service/app/__init__.py
+++ catalogue-service/app/__init__.py
@@ -1,4 +1,6 @@
 from flask import Flask
+import os
+import cloudinary
 from .models import db
 from . import config
 
@@ -6,6 +8,8 @@ def create_app():
     app = Flask(__name__)
     app.config['SQLALCHEMY_DATABASE_URI'] = config.DATABASE_CONNECTION_URI
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    cloudinary.config(cloud_name=os.getenv('CLOUD_NAME'), api_key=os.getenv('API_KEY'), 
+                  api_secret=os.getenv('API_SECRET'))
 
     db.init_app(app)
 
