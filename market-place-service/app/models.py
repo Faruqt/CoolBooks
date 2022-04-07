be4a585a55bb68950fcc1ef473786f782bca3e69
Faruq <faruqabdulsalam@yahoo.com>
2022-04-08 00:26:11 +0100

    Add new attributes to Market db class model

--- market-place-service/app/models.py
+++ market-place-service/app/models.py
@@ -6,10 +6,13 @@ class Market(db.Model):
 	__tablename__ = 'market'
 	
 	id = db.Column(db.Integer, primary_key=True)
+	user_id = db.Column(db.Integer)
 	title = db.Column(db.String(255))
 	author = db.Column(db.String(255))
 	descr = db.Column(db.String(255))
 	cover_pic = db.Column(db.String(255))
+	type = db.Column(db.String(255))
+	wanted_type = db.Column(db.String(255))
 
 	def __repr__(self):
 		return '<{} Book>'.format(self.title)
