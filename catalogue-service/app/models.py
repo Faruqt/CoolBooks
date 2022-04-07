ab197d911f410348c1aa7ad0e73291e6e75771a2
Faruq <faruqabdulsalam@yahoo.com>
2022-04-08 00:01:43 +0100

    Create new attributes for Catalgue model class

--- catalogue-service/app/models.py
+++ catalogue-service/app/models.py
@@ -5,4 +5,19 @@ class Catalogue(db.Model):
 	__tablename__ = 'catalogue'
 	
 	id = db.Column(db.Integer, primary_key=True)
-	book = db.Column(db.String(255), unique=True, nullable=False)
+	book_title = db.Column(db.String(255), unique=True)
+	author = db.Column(db.String(255))
+	description = db.Column(db.String(255))
+	cover_pic = db.Column(db.String(255))
+
+	def __repr__(self):
+		return '<{} Book>'.format(self.title)
+		
+	def to_json(self):
+		return {
+				'id': self.id,
+				'title': self.book_title,
+				'author': self.author,
+				'description': self.description,
+				'cover_pic': self.cover_pic
+			}
