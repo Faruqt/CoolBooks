f78ee5c612c75d05cd7220e83a976772a257ca0f
Faruq <faruqabdulsalam@yahoo.com>
2022-04-08 00:25:24 +0100

    Change network

--- catalogue-service/app/config.py
+++ catalogue-service/app/config.py
@@ -9,11 +9,3 @@ host = os.environ['POSTGRES_HOST']
 database = os.environ['POSTGRES_DB']
 port = os.environ['POSTGRES_PORT']
 DATABASE_CONNECTION_URI = f'postgresql+psycopg2://{user}:{password}@{host}:{port}/{database}'
-
-
-
-
-
-
-
-
