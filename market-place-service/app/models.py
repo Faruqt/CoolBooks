e60fe8db50d365f39f7a3751e3ac2304dfd4bae3
Faruq <faruqabdulsalam@yahoo.com>
2022-04-08 11:19:23 +0100

    Create to_json method in ExchangeRequest model class

--- market-place-service/app/models.py
+++ market-place-service/app/models.py
@@ -2,17 +2,31 @@ import flask_sqlalchemy
 from datetime import datetime
 
 db = flask_sqlalchemy.SQLAlchemy()
-class Market(db.Model):
-	__tablename__ = 'market'
+
+class ExchangeRequest(db.Model):
+	__tablename__ = 'exchange'
 	
 	id = db.Column(db.Integer, primary_key=True)
 	user_id = db.Column(db.Integer)
-	title = db.Column(db.String(255))
+	book_title = db.Column(db.String(255))
 	author = db.Column(db.String(255))
-	descr = db.Column(db.String(255))
+	description = db.Column(db.String(255))
 	cover_pic = db.Column(db.String(255))
 	type = db.Column(db.String(255))
 	wanted_type = db.Column(db.String(255))
+	request_matched = db.Column(db.Boolean, default=False)
 
 	def __repr__(self):
 		return '<{} Book>'.format(self.title)
+
+	def to_json(self):
+		return {
+				'id': self.id,
+				'user_id':self.user_id,
+				'title': self.book_title,
+				'author': self.author,
+				'description': self.description,
+				'cover_pic': self.cover_pic,
+				'type_tag':self.type,
+				'wanted_type':self.wanted_type
+			}
