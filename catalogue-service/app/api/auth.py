6b93fcc9ed108e00fed5812536c8e09eb5d16798
Faruq <faruqabdulsalam@yahoo.com>
2022-04-08 11:17:46 +0100

    Use try-except condition for getting user-client

--- catalogue-service/app/api/auth.py
+++ catalogue-service/app/api/auth.py
@@ -6,7 +6,11 @@ class UserClient:
         headers = {
             'Authorization': token
         }
-        response = requests.request(method="GET", url='http://cbooks_user-service:5001/api/user', headers=headers)
+        try:
+            response = requests.request(method="GET", url='http://cbooks_user-service:5001/api/user', headers=headers)
+        except requests.exceptions.ConnectionError:
+            return 'network error'
+        
         if response.status_code == 401 or response.status_code == 404:
             return False
         user = response.json()
